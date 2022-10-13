b11c8224b71be843b10d2099a3f4d80930a2df54
federossi93 <rossifederico0993@gmail.com>
2022-10-13 13:51:11 +0200

    end

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('ciao');
+
+let listaSpesa = ['carote','pasta','pane','formaggi']
+
+const ul = document.querySelector('ul')
+
+
+
+
+let i = 0
+
+while (i < listaSpesa.length) {
+    let lista = listaSpesa[i]
+    console.log(lista);
+
+    //primo metodo
+    //const li = `<li>${lista}</li>`
+    //ul.insertAdjacentHTML('beforeend',li)
+
+    const li = document.createElement('li')
+    li.append(lista)
+    ul.append(li)
+
+    i++
+}
+
